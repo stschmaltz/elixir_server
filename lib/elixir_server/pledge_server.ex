103eb45ee895cdfc6ae6f98c831631641252e498
shane schmaltz <stschmaltz@gmail.com>
2024-08-14 21:00:45 -0600

    finish lessons 25+26: Refactor stateful servers to use genserver

--- lib/elixir_server/pledge_server.ex
+++ lib/elixir_server/pledge_server.ex
@@ -1,68 +1,106 @@
 defmodule ElixirServer.PledgeServer do
-  @process_name :pledge_server
+  @name :pledge_server
+
+  use GenServer
+
+  defmodule State do
+    defstruct cache_size: 3, pledges: []
+  end
 
   # Client Interface
 
   def start do
     IO.puts("Starting the pledge server...")
-    pid = spawn(__MODULE__, :listen_loop, [[]])
-    Process.register(pid, @process_name)
-
-    pid
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def create_pledge(name, amount) do
-    send(@process_name, {self(), :create_pledge, name, amount})
-
-    receive do
-      {:response, status} -> status
-    end
+    GenServer.call(@name, {:create_pledge, name, amount})
   end
 
   def recent_pledges do
-    send(@process_name, {self(), :recent_pledges})
-
-    receive do
-      {:response, pledges} -> pledges
-    end
+    GenServer.call(@name, :recent_pledges)
   end
 
   def total_pledged do
-    send(@process_name, {self(), :total_pledged})
-
-    receive do
-      {:response, total} -> total
-    end
+    GenServer.call(@name, :total_pledged)
   end
 
-  # Server
+  def clear do
+    GenServer.cast(@name, :clear)
+  end
 
-  def listen_loop(state) do
-    receive do
-      {sender, :create_pledge, name, amount} ->
-        {:ok, new_pledge_id} = send_pledge_to_service(name, amount)
-        most_recent_pledges = Enum.take(state, 2)
-        new_state = [{name, amount} | most_recent_pledges]
-        send(sender, {:response, new_pledge_id})
-        listen_loop(new_state)
+  def set_cache_size(size) do
+    GenServer.cast(@name, {:set_cache_size, size})
+  end
 
-      {sender, :recent_pledges} ->
-        send(sender, {:response, state})
-        listen_loop(state)
+  # Server Callbacks
 
-      {sender, :total_pledged} ->
-        total = Enum.map(state, fn {_, amount} -> amount end) |> Enum.sum()
-        send(sender, {:response, total})
-        listen_loop(state)
+  def init(state) do
+    pledges = fetch_recent_pledges_from_service()
 
-      unexpected ->
-        IO.puts("Unexpected messaged: #{inspect(unexpected)}")
-        listen_loop(state)
-    end
+    {:ok, %State{state | pledges: pledges}}
+  end
+
+  def handle_cast(:clear, state) do
+    {:noreply, %State{state | pledges: []}}
+  end
+
+  def handle_cast({:set_cache_size, size}, state) do
+    {:noreply, %State{state | cache_size: size}}
+  end
+
+  def handle_call(:total_pledged, _from, state) do
+    total = Enum.map(state.pledges, &elem(&1, 1)) |> Enum.sum()
+
+    {:reply, total, state}
+  end
+
+  def handle_call(:recent_pledges, _from, state) do
+    {:reply, state.pledges, state}
+  end
+
+  def handle_call({:create_pledge, name, amount}, _from, state) do
+    {:ok, id} = send_pledge_to_service(name, amount)
+    most_recent_pledges = Enum.take(state.pledges, state.cache_size - 1)
+    cached_pledges = [{name, amount} | most_recent_pledges]
+    new_state = %State{state | pledges: cached_pledges}
+
+    {:reply, id, new_state}
+  end
+
+  def handle_info(message, state) do
+    IO.puts("Received unknown message: #{inspect(message)}")
+
+    {:noreply, state}
   end
 
   defp send_pledge_to_service(name, amount) do
-    # CODE GOES HERE TO SEND A REQUEST TO THE EXTERNAL API
+    # CODE GOES HERE TO SEND PLEDGE TO EXTERNAL SERVICE
     {:ok, "pledge-#{:rand.uniform(1000)}-#{name}-#{amount}"}
   end
+
+  defp fetch_recent_pledges_from_service do
+    # CODE GOES HERE TO FETCH RECENT PLEDGES FROM EXTERNAL SERVICE
+    [{"Wilma", 100}, {"Fred", 200}, {"Barney", 300}]
+  end
 end
+
+alias ElixirServer.PledgeServer
+
+{:ok, pid} = PledgeServer.start()
+
+send(pid, {:stop, "hammertime"})
+
+IO.inspect(PledgeServer.create_pledge("larry", 10))
+IO.inspect(PledgeServer.create_pledge("moe", 20))
+IO.inspect(PledgeServer.create_pledge("curly", 30))
+IO.inspect(PledgeServer.create_pledge("daisy", 40))
+
+PledgeServer.clear()
+
+IO.inspect(PledgeServer.create_pledge("grace", 50))
+
+IO.inspect(PledgeServer.recent_pledges())
+
+IO.inspect(PledgeServer.total_pledged())
